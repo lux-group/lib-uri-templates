814f8bcb035eb778da7f88e3209985a40b80c077
Ali Nehzat <ali.nehzat@gmail.com>
2018-05-28 09:56:14 +1000

    add limit to /offers-search

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ var definitions = {
     vendor_offers: '/api/vendor-offers{?email}',
 
     offers: '/api/offers{?page,limit,platform,region,filter,brand}{&type*}',
-    offers_search: '/api/offers-search{?q}{&brand}',
+    offers_search: '/api/offers-search{?q}{&brand,limit}',
     offer: '/api/offers/{id}{?platform,region,filter,brand}',
     offer_packages: '/api/offers/{offer_id}/packages',
     offer_package: '/api/offers/{offer_id}/packages/{id}',
