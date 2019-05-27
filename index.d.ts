e15b4f572aae8a4690ced09f41653c03456cf0fe
Rufus Post <rufuspost@gmail.com>
2019-05-27 13:56:03 +1000

    remove version argument and change to typescript (#60)
    * remove version argument and change to typescript
    
    * adds property type to definitions

---
+++ index.d.ts
@@ -0,0 +1,15 @@
+interface IExpandFunc {
+    (query: string): string;
+}
+interface ITemplate {
+    readonly rfc6570: string;
+    readonly expand: IExpandFunc;
+}
+interface IListItem {
+    readonly href: string;
+    readonly templated: boolean;
+}
+export declare function get(name: string): ITemplate;
+export declare function mock(rfc6570: string): ITemplate;
+export declare function list(): IListItem[];
+export {};
--- test/test.js
+++ test/test.js
@@ -2,45 +2,36 @@ const assert = require('assert');
 const templates = require('../index.js');
 
 describe('#get', function() {
-  const template = templates.get(1, 'properties');
 
-  describe('#version', function() {
-    it('should return the version', function() {
-      assert.equal(template.version, 1);
-    });
+  it('should return the rfc6570', function() {
+    const template = templates.get('properties');
+
+    assert.equal(template.rfc6570, '/api/properties{?id_salesforce_external,limit,page}');
   });
 
-  describe('#version', function() {
-    it('should return the rfc6570', function() {
-      assert.equal(template.rfc6570, '/api/properties{?id_salesforce_external,limit,page}');
-    });
+  it('should expand the template', function() {
+    const template = templates.get('properties');
+
+    assert.equal(template.expand({id_salesforce_external: 1}), '/api/properties?id_salesforce_external=1');
   });
 
-  describe('#expand()', function() {
-    it('should expand the template', function() {
-      assert.equal(template.expand({id_salesforce_external: 1}), '/api/properties?id_salesforce_external=1');
-    });
+  it('should expand the template (query fn builder)', function() {
+    const template = templates.get('public_offers');
+
+    assert.equal(template.expand({page: 1}), '/api/public-offers?page=1');
   });
 });
 
 describe('#mock', function() {
-  const template = templates.mock(2, '/api/properties{?id_salesforce_external,limit,page}');
+  it('should return the version', function() {
+    const template = templates.mock('/api/properties{?id_salesforce_external,limit,page}');
 
-  describe('#version', function() {
-    it('should return the version', function() {
-      assert.equal(template.version, 2);
-    });
+    assert.equal(template.version);
   });
 
-  describe('#version', function() {
-    it('should return the rfc6570', function() {
-      assert.equal(template.rfc6570, '/api/properties{?id_salesforce_external,limit,page}');
-    });
-  });
+  it('should return the rfc6570', function() {
+    const template = templates.mock('/api/properties{?id_salesforce_external,limit,page}');
 
-  describe('#expand', function() {
-    it('should return the rfc6570', function() {
-      assert.equal(typeof template.expand, 'function');
-    });
+    assert.equal(template.rfc6570, '/api/properties{?id_salesforce_external,limit,page}');
   });
 });
