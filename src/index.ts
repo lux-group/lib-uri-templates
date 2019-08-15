f750acb1c3e3d6b2f016b2bda3e27a5cdd86b2f4
Chris Nguyen <christopher.nguyen13@gmail.com>
2019-08-15 11:17:21 +1000

    0.9.10

--- src/index.ts
+++ src/index.ts
@@ -194,10 +194,7 @@ const definitions: IDefinitions = {
   flight_fare_rules:
     "/api/flights/fare-rules{?journey_id,provider,carrier,booking_class}",
 
-  flight_airports: "/api/flights/airports" + qargs(
-    "brand",
-    "region"
-  ),
+  flight_airports: "/api/flights/airports" + qargs("brand", "region"),
 
   addon: "/api/offers/{offer_id}/packages/{package_id}/addons/{id}",
   addons: "/api/offers/{offer_id}/packages/{package_id}/addons",
@@ -205,10 +202,7 @@ const definitions: IDefinitions = {
   vendor_addons: "/vendor-addons/{vendor_id}",
 
   partnership: "/api/loyalty/partnerships/{code}",
-  partnerships: "/api/loyalty/partnerships" + qargs(
-    "region",
-    "currency"
-  )
+  partnerships: "/api/loyalty/partnerships" + qargs("region", "currency")
 };
 
 function qargs(...parts: string[]) {
