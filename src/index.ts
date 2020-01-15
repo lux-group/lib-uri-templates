e302952c2cc2bbddc88f440fadb72387c9469f06
Chris Nguyen <christopher.nguyen13@gmail.com>
2020-01-16 10:24:47 +1100

    Add number of nights to calendar query params

--- src/index.ts
+++ src/index.ts
@@ -181,6 +181,7 @@ const definitions: Definitions = {
       "package_id",
       "origin",
       "region",
+      "number_of_nights",
       "number_of_adults",
       "number_of_children",
       "number_of_infants",
