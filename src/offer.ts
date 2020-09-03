cd670e0656bad7724dcdacef3e56a2ea549a0eab
Marcel Halpert <marhalpert@gmail.com>
2020-09-03 10:37:54 +1000

    add bedbank

--- src/offer.ts
+++ src/offer.ts
@@ -44,6 +44,17 @@ export const public_offer =
     "flexible_packages"
   );
 
+  export const public_bedbank_offer =
+  "/api/public-offers/bedbank/{id}" +
+  qargs(
+    "platform",
+    "region",
+    "brand",
+    "user_id",
+    "check_in",
+    "check_out",
+  );
+
 export const public_offer_packages =
   "/api/public-offers/{offer_id}/packages" +
   qargs("region", "brand", "all_packages");
