61db9e52e307231e3013577a2b395415cf89aa08
Rufus Post <rufuspost@gmail.com>
2021-12-21 11:33:07 +1300

    fix lint

--- src/search.ts
+++ src/search.ts
@@ -1,1 +1,2 @@
-export const search_trending_places = "/api/search/trending-places{?region,brand,limit}"
+export const search_trending_places =
+  "/api/search/trending-places{?region,brand,limit}";
