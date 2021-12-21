aff27a71da869e8d5c3c6e0d5231f039a5d2bef1
Rufus Post <rufuspost@gmail.com>
2021-12-21 17:06:25 +1300

    add popular place url (#147)

--- src/search.ts
+++ src/search.ts
@@ -1,2 +1,5 @@
 export const search_trending_places =
   "/api/search/trending-places{?region,brand,limit}";
+
+export const search_popular_places =
+  "/api/search/popular-places{?region,brand,limit}";
