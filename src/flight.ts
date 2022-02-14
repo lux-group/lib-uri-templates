4bb23642cc1f8e5cdb3052956f2f03fa28992e12
Alexander Sokol <reb2020@gmail.com>
2022-02-14 15:51:31 +0200

    fix(flights): update flights url

--- src/flight.ts
+++ src/flight.ts
@@ -1,5 +1,5 @@
 export const flight_single_cheapest =
-  "/api/flights/single-cheapest-search{?start_date,end_date,origin,destination,currency,number_of_adults,number_of_nights,brand,provider*,region}";
+  "/api/flights/single-cheapest-search{?start_date,end_date,origin,destination,currency,number_of_adults,number_of_children,number_of_infants,number_of_nights,brand,provider*,region}";
 
 export const flight_fare_rules =
   "/api/flights/fare-rules{?journey_id,provider,carrier,booking_class,fare_rule_type}";
