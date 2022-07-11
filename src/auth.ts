392e45ffceb758f189be22f963020f8ae5d0bc0f
jamesz96 <jamesz96.au@gmail.com>
2022-07-12 09:27:06 +1000

    add new endpoint

--- src/auth.ts
+++ src/auth.ts
@@ -3,3 +3,5 @@ export const users = "/api/users/{user_id}{?brand}";
 export const user_memberships = "/api/users/{user_id}/memberships{?brand}";
 
 export const current_user = "/api/users/current{?brand}";
+
+export const user_exists = "/api/users/exists";
