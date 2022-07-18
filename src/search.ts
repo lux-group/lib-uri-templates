0046b5941fcfee57f9bd94ecfadbc8a8a4a83926
Quan Tang <quan.tang@luxuryescapes.com>
2022-07-19 10:31:30 +1200

    [CRO-624] Update the name based on the PR feedback.

--- src/search.ts
+++ src/search.ts
@@ -4,5 +4,5 @@ export const search_trending_places =
 export const search_popular_places =
   "/api/search/popular-places{?region,brand,limit}";
 
-export const search_top_sale_places =
+export const search_top_selling_places =
   "/api/search/top-selling-places{?region,brand,country,state,limit}";
