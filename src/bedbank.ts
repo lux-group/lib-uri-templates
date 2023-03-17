b0c79b3c42971c112a580618b7f112a540ea9923
Alexander Sokol <reb2020@gmail.com>
2023-03-17 12:28:10 +0200

    feat(bedbank): add calendar url

--- src/bedbank.ts
+++ src/bedbank.ts
@@ -1,3 +1,5 @@
 export const bedbankEnquiry = "/api/bedbank/reservations/enquiry";
 
 export const bedbankReservation = "/api/bedbank/reservations";
+
+export const bedbankCalendar = "/api/bedbank/properties/{propertyId}/calendar{?region,brand,nights}";
