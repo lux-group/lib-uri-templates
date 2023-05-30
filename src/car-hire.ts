f1c43e5969f972124893641a302f8333f20bc5e0
Sergio Costa <zergiocosta@users.noreply.github.com>
2023-05-30 18:34:20 -0300

    feat(CAR-104): adds car hire service

---
+++ src/car-hire.ts
@@ -0,0 +1,3 @@
+export const search_availability = "/api/car-hire/v1/{pickUpAirportCode,pickUpDate,returnAirportCode,returnDate,driverAge,passengers}";
+
+export const reservation_get = "/api/car-hire/v1/reservation/{id,email,bookingId}"
