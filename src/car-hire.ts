6701cfd84063b417904ef3558875a26afc97b6e4
Sergio Costa <zergiocosta@users.noreply.github.com>
2023-05-30 18:37:54 -0300

    fix(CAR-104): fixes lint

--- src/car-hire.ts
+++ src/car-hire.ts
@@ -1,3 +1,5 @@
-export const search_availability = "/api/car-hire/v1/{pickUpAirportCode,pickUpDate,returnAirportCode,returnDate,driverAge,passengers}";
+export const search_availability =
+  "/api/car-hire/v1/{pickUpAirportCode,pickUpDate,returnAirportCode,returnDate,driverAge,passengers}";
 
-export const reservation_get = "/api/car-hire/v1/reservation/{id,email,bookingId}"
+export const reservation_get =
+  "/api/car-hire/v1/reservation/{id,email,bookingId}";
