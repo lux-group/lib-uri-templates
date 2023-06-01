faeaf2385b919e3d80f31460f6bac812791a58be
Sergio Costa <zergiocosta@users.noreply.github.com>
2023-06-01 14:47:14 -0300

    Update car-hire.ts

--- src/car-hire.ts
+++ src/car-hire.ts
@@ -1,4 +1,4 @@
-export const search_availability =
+export const availability_get =
   "/api/car-hire/v1/{pickUpAirportCode,pickUpDate,returnAirportCode,returnDate,driverAge,passengers}";
 
 export const reservation_get =
