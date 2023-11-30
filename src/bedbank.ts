9ae681db6028091a971bdc5194cf9e7e07e499c9
Awok3N <awok3n95@gmail.com>
2023-11-30 12:13:48 +0100

    feat(bedbank-calendar): add additional query param roomId

--- src/bedbank.ts
+++ src/bedbank.ts
@@ -3,4 +3,4 @@ export const bedbankEnquiry = "/api/bedbank/reservations/enquiry";
 export const bedbankReservation = "/api/bedbank/reservations";
 
 export const bedbankCalendar =
-  "/api/bedbank/properties/{propertyId}/calendar{?region,brand,nights}";
+  "/api/bedbank/properties/{propertyId}/calendar{?region,brand,nights,roomId}";
