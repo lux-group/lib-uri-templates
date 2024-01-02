a651bc1360f39588ce50db1dcbaf1aef24c5ed42
Harry Frankel <harry.frankel@luxuryescapes.com>
2024-01-02 16:40:07 +1100

    feat: add template priority phone numbers by type

--- src/content.ts
+++ src/content.ts
@@ -6,7 +6,8 @@ export const legal = "/legal";
 export const regions = "/regions{?brand,no_detect}";
 export const priority_phone_numbers =
   "/priority-phone-numbers{?brand,no_detect}";
-
+export const priority_phone_numbers_by_type =
+  "/priority-phone-numbers-by-type{?region,brand,type}";
 export const about_us = "/info/about-us";
 export const info = "/info";
 export const why_work_with_us = "/info/why-work-with-us";
