82675020285da530960509bf55c993c7a87f7185
greg-cumming-le <99221748+greg-cumming-le@users.noreply.github.com>
2024-11-08 15:45:55 +1300

    refine endpoint params

--- src/affilate.ts
+++ src/affilate.ts
@@ -1,4 +1,4 @@
-export const site_index = "/{?brand,locale}";
+export const site_index = "/{?region,placeId,overrideHost,checkIn,checkOut,dd}";
 
 export const site_index_search_results =
-  "/v1/results{?region,occupancy,placeIds,isDebug}";
+  "/v1/results{?region,occupancy,placeId,dd}";
