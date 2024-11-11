66a645499a5dbcd2d8a08adb0dbcdf011db441fb
greg-cumming-le <99221748+greg-cumming-le@users.noreply.github.com>
2024-11-11 16:02:10 +1300

    remove removed route

--- src/affiliate.ts
+++ src/affiliate.ts
@@ -1,5 +1,2 @@
 export const site_index =
   "/{?region,name,placeId,occupancy,overrideHost,checkIn,checkOut,dd}";
-
-export const site_index_search_results =
-  "/v1/results{?region,occupancy,placeId,dd}";
