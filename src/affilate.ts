d6231ab796f77efeb4385fea0427855753d1ef1c
greg-cumming-le <99221748+greg-cumming-le@users.noreply.github.com>
2024-11-11 14:33:04 +1300

    add name

--- src/affilate.ts
+++ src/affilate.ts
@@ -1,4 +1,5 @@
-export const site_index = "/{?region,placeId,overrideHost,checkIn,checkOut,dd}";
+export const site_index =
+  "/{?region,name,placeId,occupancy,overrideHost,checkIn,checkOut,dd}";
 
 export const site_index_search_results =
   "/v1/results{?region,occupancy,placeId,dd}";
