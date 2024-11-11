94aa7b14e5132da16e9ea8329de8ce1e04c5a178
greg-cumming-le <99221748+greg-cumming-le@users.noreply.github.com>
2024-11-12 12:10:32 +1300

    remove old params

--- src/affiliate.ts
+++ src/affiliate.ts
@@ -1,4 +1,3 @@
-export const site_index =
-  "/{?region,name,placeId,occupancy,checkIn,checkOut}";
+export const site_index = "/{?region,name,placeId,occupancy,checkIn,checkOut}";
 
 export const site_img = "/img/{imageId}/{imgOpts}";
